fc4207f61b347e974bbe9dc6cf0004353b6b7eed
Felipeaugustobezerra <58082993+Felipeaugustobezerra@users.noreply.github.com>
2024-01-14 21:10:18 +0000

    add volume control and function maped keys

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,15 +1,46 @@
+
 //select class piano keys
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
-// 
+
+//const Volume Control
+const volumeSlider = document.querySelector(".volume-slider input")
+
+//Tone folder
 let audio = new Audio("/src/tunes/a.wav");
-// 
+
+//saves the keys that exist on the piano
+let mapedKeys = [];
+
+//play tunne 
 const playTune = (key) =>{
     audio.src = `/src/tunes/${key}.wav`
     audio.play();
+    //select letters from the keyboard and add style keys
+    const clickedKey = document.querySelector(`[data-key="${key}"]`)
+    //add class active
+    clickedKey.classList.add("active");
+    //remove class active after 1500ms
+    setTimeout(()=> {
+        clickedKey.classList.remove("active");
+    },1500)
 }
 
 // Select keys values 
 pianoKeys.forEach((key) => {
     console.log(key.dataset.key)
     key.addEventListener("click",()=> playTune(key.dataset.key) )
-})
+    mapedKeys.push(key.dataset.key);
+});
+//select letters from the keyboard
+document.addEventListener("keydown",(e) =>{
+    if(mapedKeys.includes(e.key)){
+        playTune(e.key);
+    }
+    
+});
+//Volume Control
+const handleVolume = (e) => {
+    audio.volume = e.target.value;
+}
+volumeSlider.addEventListener("input",handleVolume);
+
